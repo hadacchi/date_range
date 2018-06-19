72299780d119ef5be554130e394d810f8556a4bf
hadacchi <hadacchi@users.noreply.github.com>
2018-06-19 21:11:09 +0900

    first implementation

---
+++ date_range.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+
+
+def date_range(t0, t1, dt=timedelta(days=1)):
+    '''return generator from t0 to t1 every dt.
+
+    This function returns a range generator of datetime object.
+
+    Parameters
+    ----------
+    t0 : datetime.datetime
+         A datetime object. A generator start from t0.
+    t1 : datetime.datetime
+         A datetime object. Datetime objects which the generator returns will increase until t1. t1 is never returned.
+    dt : datetime.timedelta
+         A timedelta object. Datetime objects which the generator returns will increase every dt.
+
+    Examples
+    --------
+    >>> type(date_range(datetime(2018,1,1), datetime(2018,1,4)))
+    <class 'generator'>
+    >>> list(date_range(datetime(2018,1,1), datetime(2018,1,4)))
+    [datetime.datetime(2018, 1, 1, 0, 0), datetime.datetime(2018, 1, 2, 0, 0), datetime.datetime(2018, 1, 3, 0, 0)]
+    >>> list(date_range(datetime(2018,1,1,10), datetime(2018,1,1,11), timedelta(seconds=600)))
+    [datetime.datetime(2018, 1, 1, 10, 0), datetime.datetime(2018, 1, 1, 10, 10), datetime.datetime(2018, 1, 1, 10, 20), datetime.datetime(2018, 1, 1, 10, 30), datetime.datetime(2018, 1, 1, 10, 40), datetime.datetime(2018, 1, 1, 10, 50)]
+    '''
+    t = t0
+    while t < t1:
+        val = (yield t)
+        if val is not None:
+            t = val
+        else:
+            t += dt
